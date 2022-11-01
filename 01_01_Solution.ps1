1a8a20105f71526c9092bd40e975ffbbf6b1dd04
Robert König <mailto.koenig.robert@gmail.com>
2022-11-01 20:24:58 +0100

    Solution 01_01

---
+++ 01_01_Solution.ps1
@@ -0,0 +1,37 @@
+﻿# # Teamcity configuration
+Set-StrictMode -Version Latest
+
+# Error Handler
+$ErrorActionPreference = "Stop"
+
+# Functions
+function GetPuzzleInput {
+    [CmdletBinding()]
+    param ()
+    
+    # $PSCommandPath get full path of the script 
+    $path = $PSCommandPath.Replace("Solution.ps1","Input.txt")
+    return (Get-Content -Path $path)
+}
+
+function CalculateFuel {
+    [CmdletBinding()]
+    param ($line) 
+    return [math]::Truncate((([int]$line/3)-2)/1)
+}
+
+function Main {
+    [CmdletBinding()]
+    param () 
+
+    Write-Host "[INFO] Get Puzzle Input" -ForegroundColor Green
+    $input = GetPuzzleInput
+
+    Write-Host "[INFO] Solve Puzzle"
+    foreach ($line in $input) { [int]$result += CalculateFuel $line }
+    
+    Write-Host "[INFO] Solution $result" -ForegroundColor Green
+}
+
+# Main
+Main 
