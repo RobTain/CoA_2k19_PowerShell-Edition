ba23013848cf504d66ace356f0e1e621894887cf
Robert König <mailto.koenig.robert@gmail.com>
2022-11-01 20:49:27 +0100

    Update 01_Solution.ps1
    Update with solution 01_02

--- 01_Solution.ps1
+++ 01_Solution.ps1
@@ -14,11 +14,25 @@ function GetPuzzleInput {
     return (Get-Content -Path $path)
 }
 
-function CalculateFuel {
+function CalculateFuel_Puzzle_01 {
     [CmdletBinding()]
-    param ($line) 
-    return [math]::Truncate((([int]$line/3)-2)/1)
+    param ([int]$value) 
+    return [math]::Truncate((([int]$value/3)-2)/1)
 }
+
+function CalculateFuel_Puzzle_02 {
+    [CmdletBinding()]
+    param ([int]$value)
+    
+    $result = 0
+    while ($true) {
+        $value = CalculateFuel_Puzzle_01 $value
+        if ($value -gt 0) { $result += $value } else { break }
+    }
+    return $result 
+}
+
+
 
 function Main {
     [CmdletBinding()]
@@ -27,10 +41,15 @@ function Main {
     Write-Host "[INFO] Get Puzzle Input" -ForegroundColor Green
     $input = GetPuzzleInput
 
-    Write-Host "[INFO] Solve Puzzle"
-    foreach ($line in $input) { [int]$result += CalculateFuel $line }
+    Write-Host "[INFO] Solve Puzzle 01"
+    foreach ($line in $input) { [int]$result += CalculateFuel_Puzzle_01 ([int]$line) }
+    Write-Host "[INFO] Solution Puzzle 01: $result" -ForegroundColor Green
+
+    [int]$result = 0
+    Write-Host "[INFO] Solve Puzzle 02"
+    foreach ($line in $input) { [int]$result += CalculateFuel_Puzzle_02 $line }
     
-    Write-Host "[INFO] Solution $result" -ForegroundColor Green
+    Write-Host "[INFO] Solution Puzzle 02: $result" -ForegroundColor Green
 }
 
 # Main
